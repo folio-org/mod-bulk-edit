1650ff2684350c63495f77196d75cf3620356f45
IlliaDaliek <illia_daliek@epam.com>
2021-10-12 14:30:59 +0300

    kek

--- src/main/java/org/folio/ModBulkEditApplication.java
+++ src/main/java/org/folio/ModBulkEditApplication.java
@@ -1,9 +1,11 @@
 package org.folio;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ModBulkEditApplication {
 
 	public static void main(String[] args) {
