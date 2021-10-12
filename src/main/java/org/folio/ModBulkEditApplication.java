ad861c1ef8a1b85b8bf6185ea15865bcf1182f90
IlliaDaliek <illia_daliek@epam.com>
2021-10-12 13:34:33 +0300

    base project files

---
+++ src/main/java/org/folio/ModBulkEditApplication.java
@@ -0,0 +1,13 @@
+package org.folio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModBulkEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModBulkEditApplication.class, args);
+	}
+
+}
