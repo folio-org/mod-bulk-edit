3901c40824efc969a3e0a2664a6dc0251ade128e
Illia Daliek <60663044+IlliaDaliek@users.noreply.github.com>
2021-10-21 14:32:33 +0300

    MODBULKED-1 Project setup (#1)

---
+++ src/main/java/org/folio/ModBulkEditApplication.java
@@ -0,0 +1,15 @@
+package org.folio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ModBulkEditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModBulkEditApplication.class, args);
+	}
+
+}
